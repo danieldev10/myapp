091685e2781affa40e8b8d7b209ca6380b2126ae
Achonma Daniel Tanko <danielachonma10@gmail.com>
2022-10-07 13:31:04 +0100

    logic & UI for creating new note

--- lib/views/notes/main_view.dart
+++ lib/views/notes/main_view.dart
@@ -78,6 +78,7 @@ class _MainViewState extends State<MainView> {
                 builder: (context, snapshot) {
                   switch (snapshot.connectionState) {
                     case ConnectionState.waiting:
+                    case ConnectionState.active:
                       return const Text('Waiting for all notes...');
                     default:
                       return const CircularProgressIndicator();
